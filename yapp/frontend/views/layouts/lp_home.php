e08d76e0f17e29342a48e6acffeb9de9466ce32c
Quadrosh <quadrosh@gmail.com>
2018-05-11 17:19:21 +0300

    favicon2

--- yapp/frontend/views/layouts/lp_home.php
+++ yapp/frontend/views/layouts/lp_home.php
@@ -25,6 +25,10 @@ frontend\assets\AppAsset::register($this);
     <meta name="description" content="<?= Yii::$app->view->params['meta']['description'] ?>">
     <meta name="keywords" content="<?= Yii::$app->view->params['meta']['keywords'] ?>">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
     <?php $this->head() ?>
     <?php include_once("stat_google.php") ?>
 </head>
